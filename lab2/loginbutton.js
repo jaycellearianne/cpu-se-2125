b0bf2d39a513f68fa0dbedbe4626b67279b218fc
yahneh <90144546+jaycellearianne@users.noreply.github.com>
2022-09-16 08:48:18 +0700

    Add files via upload

---
+++ lab2/loginbutton.js
@@ -0,0 +1,34 @@
+const loadButton = () => {
+
+    const buttonLogin = document.querySelector('#login');
+
+
+    buttonLogin.onclick = () => {
+
+        const input_Username = document.querySelector('#Username').value;
+        const input_password = document.querySelector('#Password').value;
+
+
+        if (!input_Username) {
+            alert("Input your email");
+            return;
+        }
+        else if (input_Username.length < 3) {
+            alert("Username must not be less than 3");
+            return;
+
+        }
+
+        if (!input_password) {
+            alert("Input your password");
+        }
+        else if (input_password.length < 3)
+            alert("Password must not be less than 3")
+        return;
+
+    }
+   
+}
+
+window.onload = loadButton;
+
